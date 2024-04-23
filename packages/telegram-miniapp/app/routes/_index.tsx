4dd4d8f69305a2988dff026ab689b184d9db0219
Nikolai Maslak <nikolai.maslak@brightpattern.com>
2024-04-24 04:14:30 +0800

    chat id

--- packages/telegram-miniapp/app/routes/_index.tsx
+++ packages/telegram-miniapp/app/routes/_index.tsx
@@ -1,39 +1,33 @@
-import { LoaderFunctionArgs, json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
 import { useEffect, useState } from "react";
 
-export const loader = async ({
-  request,
-}: LoaderFunctionArgs) => {
-  fetch(`http://localhost:${process.env.BOT_WEBHOOK_PORT}/webhook/message`)
-    .catch(() => {})
-
-  const url = new URL(request.url);
-  const queryParams = Object.fromEntries(url.searchParams);
-
-  return json({queryParams})
-};
-
-
 export default function Index() {
-  const { queryParams } = useLoaderData<typeof loader>();
-
-  const [someshit, setSomeshit] = useState('')
+  // eslint-disable-next-line
+  const [someshit, setSomeshit] = useState<any>({})
   useEffect(() => {
     if (window) {
       // eslint-disable-next-line
       // @ts-ignore
-      setSomeshit(window.Telegram.WebApp.initData)
+      const raw = window.Telegram.WebApp.initData;
+      const split = raw.split('&')
+      // eslint-disable-next-line
+      const data = split.reduce((acc: any, item: string) => {
+        const [key, value] = item.split('=')
+        acc[key] = value
+        return acc
+      }, {})
+
+      data.user = JSON.parse(data.user || '{}')
+
+      setSomeshit(data)
     }
   })
 
   return (
     <div>
       <pre>
-        {JSON.stringify(queryParams, null, 2)}
+        {JSON.stringify(someshit, null, 2)}
       </pre>
 
-      {someshit}
       Супер Игра
     </div>
   );
