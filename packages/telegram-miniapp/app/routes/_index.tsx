ac429c589027471d13f490459f2381271305dd85
Nikolai Maslak <nikolai.maslak@brightpattern.com>
2024-04-24 03:35:02 +0800

    query params in game

--- packages/telegram-miniapp/app/routes/_index.tsx
+++ packages/telegram-miniapp/app/routes/_index.tsx
@@ -1,14 +1,27 @@
-export const loader = async () => {
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader = async ({
+  request,
+}: LoaderFunctionArgs) => {
   fetch(`http://localhost:${process.env.BOT_WEBHOOK_PORT}/webhook/message`)
     .catch(() => {})
 
-  return null
+  const url = new URL(request.url);
+  const queryParams = Object.fromEntries(url.searchParams);
+
+  return json({queryParams})
 };
 
 
 export default function Index() {
+  const { queryParams } = useLoaderData<typeof loader>();
+
   return (
     <div>
+      <pre>
+        {JSON.stringify(queryParams, null, 2)}
+      </pre>
       Супер Игра
     </div>
   );
