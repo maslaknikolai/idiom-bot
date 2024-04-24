068e122e5b1d32139b17e2999c95f8c3c88e589a
Nikolai Maslak <nikolai.maslak@brightpattern.com>
2024-04-25 05:36:10 +0800

    Game starter

--- packages/telegram-miniapp/app/routes/_index.tsx
+++ packages/telegram-miniapp/app/routes/_index.tsx
@@ -1,6 +1,6 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { LoaderFunctionArgs, json } from '@remix-run/node';
-import { useLoaderData, useNavigate } from '@remix-run/react';
+import { Form, useLoaderData, useNavigate } from '@remix-run/react';
 
 // hack to make it work
 // eslint-disable-next-line
@@ -73,6 +73,8 @@ export default function Index() {
   const { username, error, groupChatId } = useLoaderData<typeof loader>();
   const navigate = useNavigate();
 
+  const [isClient, setIsClient] = useState(false);
+
   useEffect(() => {
     if (Telegram?.WebApp?.initData && Telegram.WebApp.initDataUnsafe.start_param) {
       navigate(`/?initData=${encodeURIComponent(Telegram?.WebApp?.initData)}&groupChatId=${Telegram.WebApp.initDataUnsafe.start_param}`);
@@ -86,13 +88,12 @@ export default function Index() {
         navigate(`/?initData=${encodeURIComponent(initData)}&groupChatId=${newGroupChatId}`);
       }
     }
+
+    setIsClient(true);
   }, [navigate]);
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Супер Игра</h1>
-      <br />
-
+    <div className="px-2 py-4">
       {!username && !error && !groupChatId && (
         <div>Loading...</div>
       )}
@@ -101,11 +102,52 @@ export default function Index() {
         <div>Error: {error}</div>
       )}
 
-      {username && groupChatId && (
-        <div>
-          <div>Username: {username}</div>
-        </div>
+      {isClient && !error && username && groupChatId && (
+        <Form>
+          <input type="hidden" name="username" value={username} />
+          <input type="hidden" name="groupChatId" value={groupChatId} />
+          <input type="hidden" name="groupChatId" value={encodeURIComponent(Telegram?.WebApp?.initData)} />
+          <Game username={username} />
+        </Form>
       )}
+    </div>
+  );
+}
+
+function Game({
+  username
+}: {
+  username: string
+}) {
+  const [step, setStep] = useState(0);
+
+  return (
+    <div>
+      <h1 className='flex'>
+        <span className="text-sm font-bold bg-black text-[#f17a2d] inline-block px-4 py-1 rotate-[-5deg]">
+          ИДИОМЫ
+        </span>
+
+        <p className='pl-1'>Привет, {username}!</p>
+      </h1>
+
+      <div className="mt-4">
+        <button
+          className="
+            bg-[#f17a2d] text-white px-4 py-2 mt-2 transition-shadow duration-300
+            ease-in-out
+            shadow-[1px_1px_0_0_black,2px_2px_0_0_black,3px_3px_0_0_black,4px_4px_0_0_black]
+            border-2
+            border-black
+            active:shadow-[1px_1px_0_0_black,2px_2px_0_0_black]
+            active:bg-[#ff7b23]
+          "
+          onClick={() => setStep(step + 1)}
+          type='button'
+        >
+          Начать
+        </button>
+      </div>
     </div>
   );
 }
