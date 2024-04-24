a5d91a4b8ef20d6c5bf31b376e78ade8bafee107
Nikolai Maslak <nikolai.maslak@brightpattern.com>
2024-04-25 02:51:35 +0800

    test

--- packages/telegram-miniapp/app/routes/_index.tsx
+++ packages/telegram-miniapp/app/routes/_index.tsx
@@ -1,52 +1,94 @@
-import { useEffect, useState } from "react";
+import { useEffect } from 'react';
+import { LoaderFunctionArgs, json } from '@remix-run/node';
+import { useLoaderData, useNavigate } from '@remix-run/react';
+
 
 // hack to make it work
 // eslint-disable-next-line
 import * as no from 'telegram-webapps'
 
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const urlParams = new URL(request.url).searchParams;
+  const initDataEncoded = urlParams.get('initData');
+  const groupChatId = urlParams.get('groupChatId');
+
+  if (!initDataEncoded || !groupChatId) {
+    return json({
+      error: null,
+      username: null,
+      groupChatId: null
+    }, { status: 400 });
+  }
+
+  const initData = decodeURIComponent(initDataEncoded);
+
+  return json({
+    error: null,
+    username: initData,
+    groupChatId
+  })
+
+  // // Parse and validate the initData if necessary
+  // try {
+  //   const initDataObj = JSON.parse(initData);
+  //   const userId = initDataObj.user?.id;
+  //   const groupChatId = initDataObj.chat?.id;
+  //   const botToken = process.env.TELEGRAM_BOT_TOKEN;
+
+  //   if (!userId || !groupChatId) {
+  //     return json({ error: 'User ID and Chat ID are required from initData', username: null }, { status: 400 });
+  //   }
+
+  //   const apiUrl = `https://api.telegram.org/bot${botToken}/getChatMember?chat_id=${groupChatId}&user_id=${userId}`;
+  //   const response = await fetch(apiUrl);
+  //   const data = await response.json();
+  //   if (data.ok && data.result) {
+  //     return json({ username: data.result.user.username, error: null });
+  //   }
+  //   return json({ error: 'No username found', username: null });
+
+  // } catch (error) {
+  //   return json({ error: 'Failed to parse or fetch data from Telegram API', username: null}, { status: 500 });
+  // }
+};
+
 export default function Index() {
-  const [userId, setUserId] = useState<number | undefined>(undefined)
-  const [groupChatId, setGroupChatId] = useState<number | undefined>(undefined)
+  const { username, error } = useLoaderData<typeof loader>();
+  const navigate = useNavigate();
 
+  // First time user
   useEffect(() => {
+    if (Telegram?.WebApp?.initData && Telegram.WebApp.initDataUnsafe.start_param) {
+      navigate(`/?initData=${Telegram?.WebApp?.initData}&groupChatId=${Telegram.WebApp.initDataUnsafe.start_param}`);
+    }
+
+    // For development purposes
     if (window) {
-      setUserId(Telegram.WebApp.initDataUnsafe.user?.id)
-      setGroupChatId(
-        Telegram.WebApp.initDataUnsafe.start_param
-          ? Number(Telegram.WebApp.initDataUnsafe.start_param)
-          : undefined
-      )
-
-
       // eslint-disable-next-line
       // @ts-ignore
-      window.huj = (
-        // eslint-disable-next-line
-        v: {a?: any, b?: any}
-      ) => {
-        if ('a' in v) setUserId(v.a)
-        if ('b' in v) setGroupChatId(v.b)
+      window.devInit = (initData: string, groupChatId:  string) => {
+        navigate(`/?initData=${initData}&groupChatId=${groupChatId}`);
       }
     }
-  }, [
-    setUserId,
-    setGroupChatId
-  ])
+  }, [navigate]);
+
 
   return (
     <div>
-      <h1 className="text-3xl font-bold">
-        Супер Игра
-      </h1>
-
+      <h1 className="text-3xl font-bold">Супер Игра</h1>
       <br />
 
-      {!userId || !groupChatId ? (
-        <div>Для начала игры перейдите в чат с ботом</div>
-      ) : (
+      {!username && !error && (
+        <div>Loading...</div>
+      )}
+
+      {error && (
+        <div>Error: {error}</div>
+      )}
+
+      {username && (
         <div>
-          <div>Ваш ID: {userId}</div>
-          <div>Группа: {groupChatId}</div>
+          <div>Username: {username}</div>
         </div>
       )}
     </div>
