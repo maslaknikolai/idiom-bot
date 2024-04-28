9ebae623f1897e1cdecd5f66903124e56afc369a
Nikolai Maslak <nikolai.maslak@brightpattern.com>
2024-04-28 21:54:37 +0800

    Dev data from query

--- packages/telegram-miniapp/src/App.tsx
+++ packages/telegram-miniapp/src/App.tsx
@@ -42,13 +42,8 @@ function App() {
     fetchIdiom()
 
     async function fetchIdiom() {
-      const initData = import.meta.env.MODE === 'development'
-        ? 'user=%7B%22id%22%3A734712562%2C%22first_name%22%3A%22Nikolai%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22imadeniko%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D&chat_instance=-1792476549670012747&chat_type=group&start_param=-4145745049&auth_date=1713986458&hash=49c9661a59b05f0deead096b4edd68cff57d1cc9fdeeef1de14ab87623c630c9'
-        : Telegram?.WebApp?.initData
-
-      const groupChatId = import.meta.env.MODE === 'development'
-        ? -4178714652
-        : Telegram.WebApp.initDataUnsafe.start_param;
+      const initData = new URLSearchParams(window.location.search).get('initData') || Telegram.WebApp?.initData
+      const groupChatId = new URLSearchParams(window.location.search).get('groupChatId') || Telegram.WebApp.initDataUnsafe.start_param;
 
       const response = await fetch(`${import.meta.env.VITE_API_URL || ''}/api/game-data?initData=${encodeURIComponent(initData)}&groupChatId=${groupChatId}`);
       const data = await response.json();
